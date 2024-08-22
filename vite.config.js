5995347c2974e4bc24a8ef3181615ed33fa6ada2
YevhenKharchenko <evgenvalerych@gmail.com>
2024-08-22 20:58:35 +0300

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,58 @@
+import { defineConfig } from 'vite';
+import glob from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { globalStylesOptions } from './global.styles';
+
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+    },
+    root: 'src',
+    build: {
+      sourcemap: true,
+
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: 'commonHelpers.js',
+        },
+      },
+      outDir: '../dist',
+    },
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/**.html']),
+      ViteImageOptimizer({
+        exclude: /^sprite.svg$/,
+        png: {
+          quality: 60,
+        },
+        jpeg: {
+          quality: 60,
+        },
+        jpg: {
+          quality: 60,
+        },
+        webp: {
+          quality: 60,
+        },
+      }),
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: globalStylesOptions,
+        },
+      },
+    },
+    base: '/vanilla-vite-template'
+  };
+});
