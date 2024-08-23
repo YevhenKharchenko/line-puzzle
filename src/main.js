b03d2f264a32f8421ee95b897be3b79efa89100c
YevhenKharchenko <evgenvalerych@gmail.com>
2024-08-23 19:59:21 +0300

    add gallery

--- src/main.js
+++ src/main.js
@@ -13,3 +13,32 @@ closeContextBtn.addEventListener('click', onCloseContextButtonClick);
 function onCloseContextButtonClick() {
   contextMenu.classList.remove('is-open');
 }
+
+let currentIndex = 0;
+const items = document.querySelectorAll('.gallery-item');
+const dots = document.querySelectorAll('.dot');
+
+document.getElementById('leftArrow').addEventListener('click', () => {
+  changeItem(-1);
+});
+
+document.getElementById('rightArrow').addEventListener('click', () => {
+  changeItem(1);
+});
+
+function changeItem(direction) {
+  items[currentIndex].classList.remove('active');
+  dots[currentIndex].classList.remove('active');
+
+  currentIndex = (currentIndex + direction + items.length) % items.length;
+
+  items[currentIndex].classList.add('active');
+  dots[currentIndex].classList.add('active');
+}
+
+function initCarousel() {
+  items[currentIndex].classList.add('active');
+  dots[currentIndex].classList.add('active');
+}
+
+initCarousel();
