81b168de1c62c639f87484918d5c3789e981847f
YevhenKharchenko <evgenvalerych@gmail.com>
2024-08-24 12:58:12 +0300

    add play section

--- src/main.js
+++ src/main.js
@@ -42,3 +42,34 @@ function initCarousel() {
 }
 
 initCarousel();
+
+const playCarousel = document.querySelector('.play-carousel');
+const playItems = document.querySelectorAll('.play-item');
+const playDots = document.querySelectorAll('.play-dot');
+let currentPlayIndex = 0;
+
+document.getElementById('playLeftArrow').addEventListener('click', () => {
+  changePlayItem(-1);
+});
+
+document.getElementById('playRightArrow').addEventListener('click', () => {
+  changePlayItem(1);
+});
+
+function changePlayItem(direction) {
+  playItems[currentPlayIndex].classList.remove('play-active');
+  playDots[currentPlayIndex % playDots.length].classList.remove('play-active');
+
+  currentPlayIndex =
+    (currentPlayIndex + direction + playItems.length) % playItems.length;
+
+  playItems[currentPlayIndex].classList.add('play-active');
+  playDots[currentPlayIndex % playDots.length].classList.add('play-active');
+}
+
+function initPlayCarousel() {
+  playItems[currentPlayIndex].classList.add('play-active');
+  playDots[currentPlayIndex % playDots.length].classList.add('play-active');
+}
+
+initPlayCarousel();
