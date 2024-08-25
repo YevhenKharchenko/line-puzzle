fc0476f92f6ce7b131972273f016f9a5aab5d183
YevhenKharchenko <evgenvalerych@gmail.com>
2024-08-25 19:14:16 +0300

    add fixed header

--- src/main.js
+++ src/main.js
@@ -127,3 +127,18 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//   anchor.addEventListener('click', function (e) {
+//     e.preventDefault();
+
+//     const targetId = this.getAttribute('href').substring(1);
+//     const targetElement = document.getElementById(targetId);
+//     const headerHeight = document.querySelector('header').offsetHeight;
+
+//     window.scrollTo({
+//       top: targetElement.offsetTop - headerHeight,
+//       behavior: 'smooth',
+//     });
+//   });
+// });
