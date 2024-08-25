d92670cf2166b4eb05c581550cf3898964b30d65
YevhenKharchenko <evgenvalerych@gmail.com>
2024-08-25 22:19:58 +0300

    add faq animation

--- src/main.js
+++ src/main.js
@@ -128,17 +128,42 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
-// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-//   anchor.addEventListener('click', function (e) {
-//     e.preventDefault();
+// const faqBtns = document.querySelectorAll('.faq-top-text');
+// faqBtns.forEach(btn => btn.addEventListener('click', onBtnClick));
 
-//     const targetId = this.getAttribute('href').substring(1);
-//     const targetElement = document.getElementById(targetId);
-//     const headerHeight = document.querySelector('header').offsetHeight;
+// function onBtnClick(e) {
+//   const faqTop = e.currentTarget;
+//   const faqContainer = faqTop.closest('.faq-list-item');
+//   faqContainer.classList.add('faq-open');
+//   const bottomText = faqContainer.querySelector('.faq-bottom-text');
+//   bottomText.classList.toggle('is-visible');
 
-//     window.scrollTo({
-//       top: targetElement.offsetTop - headerHeight,
-//       behavior: 'smooth',
-//     });
-//   });
-// });
+//   if (!bottomText.classList.contains('is-visible')) {
+//     faqContainer.classList.remove('faq-open');
+//   }
+// }
+
+const faqBtns = document.querySelectorAll('.faq-top-text');
+faqBtns.forEach(btn => btn.addEventListener('click', onBtnClick));
+
+function onBtnClick(e) {
+  const faqTop = e.currentTarget;
+  const faqContainer = faqTop.closest('.faq-list-item');
+
+  document.querySelectorAll('.faq-list-item').forEach(item => {
+    if (item !== faqContainer) {
+      item.classList.remove('faq-open');
+
+      item.querySelector('.faq-bottom-text').classList.remove('is-visible');
+    }
+  });
+
+  const bottomText = faqContainer.querySelector('.faq-bottom-text');
+  bottomText.classList.toggle('is-visible');
+
+  if (bottomText.classList.contains('is-visible')) {
+    faqContainer.classList.add('faq-open');
+  } else {
+    faqContainer.classList.remove('faq-open');
+  }
+}
